f818a3d7c2f13e74f012b639a13f0b278c0dcb21
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2020-12-24 02:28:02 -0300

    Update Index , Login
    Index -> Novos Browsers
    Login -> Novos TimeOuts ,  Clicks

--- index.py
+++ index.py
@@ -12,14 +12,20 @@ async def main():
     email = os.getenv("EMAIL")
     senha = os.getenv("SENHA")
     async with async_playwright() as p:
-        browser = await p.chromium.launch(executablePath="C:\Program Files\Google\Chrome\Application\chrome.exe", headless = False)
+        browser = await p.webkit.launch(headless=False)
         page = await browser.newPage()
         page.setDefaultTimeout(0)
-        access = await asyncio.wait([login(browser,page,email,senha)])
-        if access:
-            input()
+
+        access = asyncio.create_task(login(browser,page,email,senha))
+        done , pending = await asyncio.wait({access},return_when="ALL_COMPLETED")
+        
+        if access in done :
+            print("Login: Success")
             await browser.close()
             return True
+        else:
+            print("Login: Failed")
+            await browser.close()
     return True
 
 asyncio.get_event_loop().run_until_complete(main())
--- login.py
+++ login.py
@@ -2,17 +2,22 @@ import asyncio
 from playwright import async_playwright
 
 async def login(browser , page , emailUser: str , senhaUser: str) -> bool:
-    await page.goto("https://www.nike.com.br/Snkrs",waitUntil='load')
+    await page.goto("https://www.nike.com.br",waitUntil='load')
     await page.click("#anchor-acessar")
     await page.waitForTimeout(700)
-    if emailUser and senhaUser != "":
-        await page.type('input[name="emailAddress"]',emailUser,delay=150)
-        await page.waitForTimeout(500)
-        await page.type('input[name="password"',senhaUser,delay=150)
-        await page.waitForTimeout(500)
-        await page.keyboard.press('Enter')
-        await page.waitForNavigation(waitUntil='load')
-        return True
+    if emailUser or senhaUser != "":
+        try:
+            await page.type('input[name="emailAddress"]',emailUser,delay=250)
+            await page.waitForTimeout(2000)
+            await page.type('input[name="password"',senhaUser,delay=250)
+            await page.waitForTimeout(1000)
+            await page.click('#keepMeLoggedIn')
+            await page.waitForTimeout(5000)
+            await page.click('input[value="ENTRAR"')
+            await page.waitForNavigation(waitUntil='load')
+            return True
+        except TimeoutError:
+            return False
     else:
         await browser.close()
         print("Insira um email e/ou senha validos.")
